f8e13c812707216fdb518d082b03f058d14ccf86
Gagan0322 <sin12455@sheridancollege.ca>
2023-02-21 10:23:55 -0500

    i am done with creating repo.

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,15 @@
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+
+public class TestUserprofile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("enter the user id");
+         String userID = scan.next();
+         System.out.println("enter the user id");
+         String genres = scan.next();
+         
+    }
+}
